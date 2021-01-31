d09dcf14be20fee880aedd0108ebf7c08f0bb528
orthrus1775 <orthrus1775@gmail.com>
2021-01-31 18:11:06 -0500

    fixed errors

--- tool_installer.sh
+++ tool_installer.sh
@@ -130,8 +130,8 @@ install_vscode(){
 apt-get install software-properties-common apt-transport-https
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-apt-get update
-apt-get install code
+apt-get update -y
+apt-get install code -y
 }
 
 #Install WinboxExploit
