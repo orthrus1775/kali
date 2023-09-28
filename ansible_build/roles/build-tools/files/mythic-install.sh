f25620da4c5e93376c3b408c013f0d678649acd6
orthrus1775 <orthrus1775@gmail.com>
2023-09-28 11:06:56 -0500

    mythic updates

--- ansible_build/roles/build-tools/files/mythic-install.sh
+++ ansible_build/roles/build-tools/files/mythic-install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-export PATH=/opt/mythic/:$PATH
-sudo apt install docker-compose -y
-sudo mythic-cli install github https://github.com/MythicAgents/apollo
-sudo mythic-cli install github https://github.com/MythicC2Profiles/http
-sudo mythic-cli install github https://github.com/MythicC2Profiles/tcp
-sudo mythic-cli install github https://github.com/MythicC2Profiles/smb
-mythic-cli start
+
+cd /opt/mythic/
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicC2Profiles/http
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicC2Profiles/tcp
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicC2Profiles/smb
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/Apollo.git
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/merlin
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/medusa
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/Athena
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/thanatos
+sudo sed -i 's/REBUILD_ON_START="true"/REBUILD_ON_START="false"/g' /opt/mythic/.env
+sudo /opt/mythic/mythic-cli start
+sudo /opt/mythic/mythic-cli stop
