09e2a05d0da71bf94b3e4c7f8f642b656d8f41da
orthrus1775 <orthrus1775@gmail.com>
2023-12-28 17:50:56 -0600

    fixed docker compose edits

--- roles/build-tools/files/mythic-install.sh
+++ roles/build-tools/files/mythic-install.sh
@@ -10,7 +10,7 @@ sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/merli
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/medusa
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/Athena
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/thanatos
-sed -i 's/restart: always/restart: no/g' docker-compose.yml
+sed -i 's/restart: always/restart: on-failure:10/g' docker-compose.yml
 sudo /opt/mythic/mythic-cli stop
 sudo /opt/mythic/mythic-cli start
 sudo sed -i 's/REBUILD_ON_START="true"/REBUILD_ON_START="false"/g' /opt/mythic/.env
