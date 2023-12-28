216c6c28dbeddefe6cc8d2b46f11192c042febc6
orthrus1775 <orthrus1775@gmail.com>
2023-12-28 15:51:44 -0600

    fixed go issues

---
+++ roles/install-tools/files/gomod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+go install github.com/ropnop/kerbrute@latest
+go install mvdan.cc/garble@latest
+go install github.com/fatih/color@latest
+go install github.com/yeka/zip@latest
+go install github.com/josephspurrier/goversioninfo@latest
+go install github.com/Binject/debug/pe@latest
+go install github.com/awgh/rawreader@latest
+exit 0
