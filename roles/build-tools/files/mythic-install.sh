69a85b8ff74143ae6dce0c2434d9331709c6f18e
orthrus1775 <orthrus1775@gmail.com>
2024-05-05 22:42:09 -0500

    updates

--- roles/build-tools/files/mythic-install.sh
+++ roles/build-tools/files/mythic-install.sh
@@ -5,6 +5,7 @@ cd /opt/mythic/
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicC2Profiles/http
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicC2Profiles/tcp
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicC2Profiles/smb
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicC2Profiles/httpx.git
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/Apollo
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/merlin
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/medusa
@@ -12,13 +13,12 @@ sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/Athen
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/thanatos
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/arachne
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/sliver
-sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/ghostwriter
+sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/bloodhound.git
+#sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/ghostwriter
+#sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/service_wrapper.git
 #sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/scarecrow_wrapper
-sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/pickle_wrapper.git
+#sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/pickle_wrapper.git
 #sed -i 's/restart: always/restart: on-failure:10/g' docker-compose.yml
 sudo /opt/mythic/mythic-cli stop
-sudo /opt/mythic/mythic-cli start
 sudo sed -i 's/REBUILD_ON_START="true"/REBUILD_ON_START="false"/g' /opt/mythic/.env
-sudo /opt/mythic/mythic-cli start
 sleep 45
-sudo /opt/mythic/mythic-cli stop
