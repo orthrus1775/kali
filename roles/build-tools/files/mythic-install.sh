e58e9f49c593dfcc86f878937c6e6d1c493e5457
orthrus1775 <34408810+orthrus1775@users.noreply.github.com>
2024-05-07 19:19:22 -0500

    Update mythic-install.sh

--- roles/build-tools/files/mythic-install.sh
+++ roles/build-tools/files/mythic-install.sh
@@ -14,10 +14,10 @@ sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/thana
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/arachne
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/sliver
 sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/bloodhound.git
-sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/ghostwriter
-sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/service_wrapper.git
-sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/scarecrow_wrapper
-sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/pickle_wrapper.git
+#sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/ghostwriter
+#sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/service_wrapper.git
+#sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/scarecrow_wrapper
+#sudo /opt/mythic/mythic-cli install github https://github.com/MythicAgents/pickle_wrapper.git
 sed -i 's/restart: always/restart: on-failure:10/g' docker-compose.yml
 sudo sed -i 's/REBUILD_ON_START="true"/REBUILD_ON_START="false"/g' /opt/mythic/.env
 sudo /opt/mythic/mythic-cli stop
