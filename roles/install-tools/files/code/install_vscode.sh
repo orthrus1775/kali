e0725169632c876b26148a60b2fb8a7c8f63d0f2
orthrus1775 <orthrus1775@gmail.com>
2025-04-10 17:41:44 -0500

    yolo

---
+++ roles/install-tools/files/code/install_vscode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install wget gpg -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install code -y
